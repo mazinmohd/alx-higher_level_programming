11ae51083056104c8726f6ecbbe96d690ef3c2a5
mazinmohd <mazenomermazen2002@gmail.com>
2023-11-04 07:55:43 +0300

    task num 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if !sentence:
+        return 0, None
+    first_char = sentence[0]
+    length = len(sentence)
+    return length, first_char
