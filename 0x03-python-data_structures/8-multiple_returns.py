4deb840fcfe3edd94a5aacb44a2b27f4b498c81a
mazinmohd <mazenomermazen2002@gmail.com>
2023-11-04 07:58:30 +0300

    edit on task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@ def multiple_returns(sentence):
     first_char = sentence[0]
     length = len(sentence)
     if length == 0:
-        return 0, None
+        return 0, "None"
     return length, first_char
