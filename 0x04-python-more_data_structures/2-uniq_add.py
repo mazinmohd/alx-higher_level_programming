a3fd1ea228e22817832541ee18a61c434ea83399
mazinmohd <mazenomermazen2002@gmail.com>
2023-11-08 13:24:37 +0300

    third task

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pyhton3
+def uniq_add(my_list=[]):
+    new_list = []
+    sum = 0
+    for element in my_list:
+        if element in new_list:
+            pass
+        else:
+            sum += element
+            new_list.append(element)
+    return sum
