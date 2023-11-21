0e73dfa2220770031c9f1db3a4ab1ec565e8aa76
mazinmohd <mazenomermazen2002@gmail.com>
2023-11-21 12:26:16 +0300

    second task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,7 +3,14 @@
 
 
 class Square:
-    """ a square class """
+    """ a square class 
+    Attributes:
+    __size (int): size of a side of the square
+    """
     def __init__(self, size):
-        """initalizes attributes"""
+        """initalizes attributes
+        Args:
+            size (int): size of a side of the square
+            Returns: None
+        """
         self.__size = size
