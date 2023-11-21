931426181edac6c9f86db046ebae1eaa53bb360c
mazinmohd <mazenomermazen2002@gmail.com>
2023-11-21 12:38:40 +0300

    third task

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""initalize class"""
+
+
+class Square:
+    """class with private instance"""
+
+    def __init__(slef, size=0):
+        """
+        Args and size
+        """
+
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+            raise TypeError('size must be an integer')
