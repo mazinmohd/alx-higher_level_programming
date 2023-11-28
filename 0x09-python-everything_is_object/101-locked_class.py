491ed3df68742e675fade2a40f8b978ea388c7c1
mazinmohd <mazenomermazen2002@gmail.com>
2023-11-28 23:34:39 +0300

    second advance

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This program define a LockedClass
+"""
+
+
+class LockedClass():
+    """
+    This class is Locked and you can only put the attribute
+    """
+
+    __slots__ = ('first_name', )
