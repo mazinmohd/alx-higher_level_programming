bfd8a7ec9be6b5f11eb5b1e9546a24eb80389449
mazinmohd <mazenomermazen2002@gmail.com>
2023-12-05 23:12:54 +0300

    task num 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Declaration of Class"""
+
+
+class Student:
+    """Class
+    """
+    def __init__(self, first_name, last_name, age):
+        """init instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to json method"""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
