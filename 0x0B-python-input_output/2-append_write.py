a1d3d9d964f09d60c61309b0459a50f1330b30c9
mazinmohd <mazenomermazen2002@gmail.com>
2023-12-05 17:04:57 +0300

    task num 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""declaration of function"""
+
+
+def append_write(filename="", text=""):
+    """write into a file
+
+    Args:
+        filename: the name of the file.
+        text: the text we want.
+
+    Returns:
+        the number of characters.
+    """
+    with open(filename, "a", encoding="utf-8") as my_file:
+        return my_file.write(text)
+    
