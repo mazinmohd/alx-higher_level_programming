e149b1b8cb9c080f0b0fa51e59b6c6a071a4213e
mazinmohd <mazenomermazen2002@gmail.com>
2023-12-05 17:20:54 +0300

    task num 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -3,12 +3,11 @@
 import json
 
 
-def save_to_json_file(my_obj, filename):
+def load_from_json_file(filename):
     """Returns an object represented by a JSON
 
     Args:
-        my_obj: the object
         filename: the name of file
     """
     with open(filename, "w") as my_file:
-        json.dump(my_obj, my_file)
+        json.load(my_file)
