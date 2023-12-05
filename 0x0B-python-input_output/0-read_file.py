74b92d36a543a528ef82c2273a69b206713e4733
mazinmohd <mazenomermazen2002@gmail.com>
2023-12-05 16:51:45 +0300

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
         filename: the name of file.
     """
     with open(filename, encoding="utf-8") as my_file:
-        print(my_file.read())
+        print(my_file.read(), end="")
