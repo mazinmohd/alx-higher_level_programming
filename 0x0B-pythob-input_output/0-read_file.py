e6ecc0efd4d8fa58d6e9458b87889929aeb85b2a
mazinmohd <mazenomermazen2002@gmail.com>
2023-12-05 16:49:08 +0300

    task 0

---
+++ 0x0B-pythob-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""declaration of function"""
+
+
+def read_file(filename=""):
+    """read content of file
+
+    Args:
+        filename: the name of file.
+    """
+    with open(filename, encoding="utf-8") as my_file:
+        print(my_file.read())
