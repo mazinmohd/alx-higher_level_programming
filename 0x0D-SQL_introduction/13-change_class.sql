7972d9ce04623aed3c73d4cf653caeaf254e4527
mazinmohd <mazenomermazen2002@gmail.com>
2024-01-16 23:41:46 +0300

    two tasks

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,2 @@
+-- remo es all record with scroe <= 5
+DELETE FROM `second_table` WHERE `score` <= 5;
---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+-- lists the number of records
+SELECT `score`, COUNT(*) AS `number` FROM `second_table`
+GROUP BY `score` ORDER BY `number` DESC;
