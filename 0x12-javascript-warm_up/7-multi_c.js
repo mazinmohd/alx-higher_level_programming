ccb96cec4ddeeb42fad905e5784becabf3e2cd2f
mazinmohd <mazenomermazen2002@gmail.com>
2024-02-13 02:53:03 +0300

    edit on 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Missing number of occurrences');
-}
-else {
+} else {
   const num = Number(process.argv[2]);
   let i = 0;
   while (i < num) {
