474bdadbfb1eac9256d12f96d0bcb3772688614a
mazinmohd <mazenomermazen2002@gmail.com>
2024-02-13 20:54:17 +0300

    edit on task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,13 +6,14 @@ class Rectangle {
       this.height = h;
     }
   }
+
   print () {
     for (let i = 0; i < this.height; i++) {
-      let size = '';
+      let s = '';
       for (let j = 0; j < this.width; j++) {
-        size += 'X';
+        s += 'X';
       }
-      console.log(size);
+      console.log(s);
     }
   }
 }
